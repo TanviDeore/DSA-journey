d15411a3667de2ca736ae4b00fabba8d26ed85b8
Tanvi Deore <tanvi.deore1124@gmail.com>
2024-10-23 19:27:51 -0500

    twoSum

--- Solution.java
+++ Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
-//O(n)
+//O(n) 
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0;i<nums.length;i++)
         {
